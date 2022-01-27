24108b50fe7c1f6016d56eb921d15b7d19ef0941
Vinnie <vinnie.terlouw@gmail.com>
2022-01-27 11:51:10 +0100

    joe

--- src/Pages/PokemonDiscoveryPage.js
+++ src/Pages/PokemonDiscoveryPage.js
@@ -1,1 +1,28 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
+
+import PokemonBlock from "./Components/PokemonBlock";
+
+export default function PokemonDiscoveryPage() {
+  const [pokeList, setPokelist] = useState(null);
+
+  useEffect(() => {
+    async function getPokemons() {
+      const pokeResponse = await axios.get(
+        "https://pokeapi.co/api/v2/pokemon?limit=151"
+      );
+      setPokelist(pokeResponse.data.results);
+    }
+    getPokemons();
+  }, []);
+
+  return (
+    <div>
+      {pokeList ? (
+        pokeList.map((pokeObj) => <PokemonBlock name={pokeObj.name} />)
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+}
